c874f650acf306fe76ba2a1dcd760f8d8620044e
BarbyDal <bdalonso87@gmail.com>
2022-06-06 21:54:13 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import LogoImg from './img/alonsilogo.JPG';
 import './App.css';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <NavBar/>
+      <header className="App-header">        
+       <img alt="logo marca" src={ LogoImg } />
+       <h2>Alonsi e-commerce de ropa</h2>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        >         
         </a>
       </header>
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+
+
+export default function NavBar(){
+
+    return (
+<nav class="navbar navbar-expand navbar-light ">
+		<a class="navbar-brand ps-3 text-dark" href="./">  
+			 Inicio
+		</a>
+        <ul class="navbar-nav mr-auto">
+			<li class="nav-item  ml-3"> 
+				<a class="nav-link text-dark" href="./" data-abc="true">
+                 Productos
+				</a> 
+			</li>
+			<li class="nav-item  ml-3"> 
+				<a class="nav-link text-dark" href="./" data-abc="true">
+                 Contacto
+				</a> 
+			</li>
+		</ul>
+</nav>
+);
+
+
+}
